17694febdfc45bcc637a886e64856cdcb80969e7
uladzimirAAsanko <volodyasn@gmail.com>
2021-05-20 10:09:04 +0300

    smth

--- src/main/java/by/sanko/spark/two/entity/main/Main.java
+++ src/main/java/by/sanko/spark/two/entity/main/Main.java
@@ -47,14 +47,14 @@ public class Main {
         List<Row> data = dataset.selectExpr("CAST(id AS LONG)","CAST(srch_ci AS STRING)", "CAST(srch_co AS STRING)").collectAsList();
         List<Row> answer = new ArrayList<>();
         for(Row row : data) {
-            Long id = Long.parseLong(row.getString(0));
+            String id = row.getString(0);
             String checkIN = row.getString(1);
             String checkOUT = row.getString(2);
             int stayType = StayType.calculateType(checkIN, checkOUT).getStayID();
             answer.add(RowFactory.create(id,stayType));
         }
         List<org.apache.spark.sql.types.StructField> structs = new ArrayList<>();
-        structs.add(DataTypes.createStructField("id", DataTypes.LongType,false));
+        structs.add(DataTypes.createStructField("id", DataTypes.StringType,false));
         structs.add(DataTypes.createStructField("stay_type", DataTypes.IntegerType,false));
         StructType structures = DataTypes.createStructType(structs);
         Dataset<Row> answerAtAll = sparkSession.createDataFrame(answer, structures);
